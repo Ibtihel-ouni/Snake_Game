362147076bf0152aa183ab6e6ec0c796d71ef5e6
maduck <utti+github@uttisoft.de>
2020-10-20 19:45:19 +0200

    refactor: make random snack more readable

--- Snake.py
+++ Snake.py
@@ -140,14 +140,12 @@ def redraw_window(surface):
 
 def random_snack(rows, item):
     positions = item.body
+    x = random.randrange(rows)
+    y = random.randrange(rows)
 
-    while True:
+    while (x, y) in [z.pos for z in positions]:
         x = random.randrange(rows)
         y = random.randrange(rows)
-        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
-            continue
-        else:
-            break
     return x, y
 
 
